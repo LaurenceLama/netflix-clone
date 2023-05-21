f2d6888b764749bccde0a4068f654bc00e36c3fc
LaurenceLama <laurencelamaFSE@gmail.com>
2023-05-21 23:32:14 +0800

    Functions for login page, bground css malfunctions

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,17 +1,34 @@
 import Head from "next/head";
 import Image from "next/image";
+import { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+interface Inputs {
+  email: string;
+  password: string;
+}
 
 function login() {
+  const [login, setLogin] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
   return (
     <div className="relative flex h-screen w-screen flex-col bg=black md:items-center md:justify-center md:bg-transparent">
       <Head>
         <title>Netflix</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href=".../favicon.ico" />
       </Head>
       <img
         src="https://rb.gy/p2hphi"
         alt="rawr"
-        className="-z-10 !hidden opacity-60 sm:!inline object-fill"
+        className="-z-10 !hidden opacity-60 sm:!inline object-cover"
+        // layout='fill' and objectFit='cover' dont work help
       />
 
       <img
@@ -21,11 +38,19 @@ function login() {
         height={150}
       />
 
-      <form className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14"
+      >
         <h1 className="text-4xl font-semibold">Sign in</h1>
         <div className="space-y-4">
           <label className="inline-block w-full">
-            <input type="email" className="input" placeholder="Email" />
+            <input
+              type="email"
+              className="input"
+              placeholder="Email"
+              {...register("email", { required: true })}
+            />
           </label>
           <label className="inline-block w-full">
             <input type="password" className="input" placeholder="password" />
@@ -37,8 +62,10 @@ function login() {
         </button>
 
         <div className="text-[gray]">
-          New to Netflix?{' '}
-          <button type='submit' className="text-white hover:underline">Sign up now</button>
+          New to Netflix?{" "}
+          <button type="submit" className="text-white hover:underline">
+            Sign up now
+          </button>
         </div>
       </form>
     </div>
