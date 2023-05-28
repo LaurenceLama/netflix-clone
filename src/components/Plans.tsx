2c91b7dfe3c99763833e390ed9930d927bebc858
LaurenceLama <laurencelamaFSE@gmail.com>
2023-05-28 23:33:44 +0800

    Initializing stripe webhooks (pending)

--- src/components/Plans.tsx
+++ src/components/Plans.tsx
@@ -1,7 +1,11 @@
 import Head from "next/head";
 import Link from "next/link";
+import useAuth from "../../hooks/useAuth";
+import { CheckIcon } from "@heroicons/react/20/solid";
 
 function Plans() {
+  const { logout } = useAuth();
+
   return (
     <div>
       <Head>
@@ -21,13 +25,45 @@ function Plans() {
         </Link>
         <button
           className="text-lg font-medium hover:underline"
-        //   onClick={logout}
+          onClick={logout}
         >
           Sign Out
         </button>
       </header>
+
+      <main className="mx-auto max-w-5xl px-5 pt-28 pb-12 transition-all md:px-10">
+        <h1 className="mb-3 text-3xl font-medium">
+          Choose the plan that's right for you
+        </h1>
+        <ul>
+          <li className="flex items-center gap-x-2 text-lg">
+            <CheckIcon className="h-7 w-7 text-[#E50914]" /> Watch all you want.
+            Ad-free.
+          </li>
+          <li className="flex items-center gap-x-2 text-lg">
+            <CheckIcon className="h-7 w-7 text-[#E50914]" /> Recommendations
+            just for you.
+          </li>
+          <li className="flex items-center gap-x-2 text-lg">
+            <CheckIcon className="h-7 w-7 text-[#E50914]" /> Change or cancel
+            your plan anytime.
+          </li>
+        </ul>
+
+        <div className="mt-4 flex flex-col space-y-4">
+          <div className="flex w-full items-center justify-end self-end md:w-3/5">
+            <div className="planBox">standerd</div>
+            <div className="planBox">standerd</div>
+            <div className="planBox">standerd</div>
+          </div>
+
+          {/* <Table products={products} selectedPlan={selectedPlan} /> */}
+
+          <button>subcribe</button>
+        </div>
+      </main>
     </div>
   );
 }
 
-export default Plans
+export default Plans;
