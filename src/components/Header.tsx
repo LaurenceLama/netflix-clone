eb18ddb73d06e8e629949327083805a1abade3ff
LaurenceLama <laurencelamaFSE@gmail.com>
2023-06-05 11:59:04 +0800

    Initializing account page, pending membership

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,6 @@ import useAuth from "../../hooks/useAuth";
 
 function Header() {
   const [isScrolled, setIsScrolled] = useState(false)
-  const { logout } = useAuth();
 
   useEffect(() => {
     const handleScroll = () => {
@@ -51,14 +50,13 @@ function Header() {
         <MagnifyingGlassIcon className="hidden h-6 w-6 sm:inline" />
         <p className="hidden lg:inline">Kids</p>
         <BellIcon className="h-6 w-6" />
-        {/* <Link href="/account"> */}
+        <Link href="/account">
           <img
-            onClick={logout}
             src="https://rb.gy/g1pwyx"
             alt=""
             className="cursor-pointer rounded"
           />
-        {/* </Link> */}
+        </Link>
       </div>
     </header>
   );
